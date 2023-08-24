ada81c233b8231500f7b6be31d942a03a1466f82
gfg7 <nakonechnaya040702@gmail.com>
2023-08-24 17:33:44 +0500

    add equifax score

--- AppOptions.cs
+++ AppOptions.cs
@@ -9,9 +9,9 @@ namespace PackageRequest
         public bool LogIncomming { get; set; }
         public bool Loging { get; set; }
         public string LogsPath { get; set; }
-        public int MaxRollingFiles {get;set;}
-        public long FileSizeLimitBytes {get;set;}
-        public LogLevel MinLevel {get;set;}
+        public int MaxRollingFiles { get; set; }
+        public long FileSizeLimitBytes { get; set; }
+        public LogLevel MinLevel { get; set; }
         public int SleepNbch { get; set; }
         public int SleepExperian { get; set; }
         public int SleepEquifax { get; set; }
@@ -19,6 +19,7 @@ namespace PackageRequest
         public string FtpDirectoryOut { get; set; }
         public string RKK_NbchResponcePath { get; set; }
         public int? NbchRetryCount { get; set; }
+        public int? EquifaxScoringRetryCount { get; set; }
         public string RKK_NbchTakenResponcePath { get; set; }
         public string RKK_NbchUsedResponcePath { get; set; }
         public string RKK_EiResponcePath { get; set; }
@@ -27,6 +28,8 @@ namespace PackageRequest
         public string RKK_EquifaxUsedResponcePath { get; set; }
         public string ScoringEiResponcePath { get; set; }
         public string ScoringEquifaxResponcePath { get; set; }
-        public bool EquifaxEnabled {get;set;}
+        public string ScoringEquifaxRTakenResponcePath { get; set; }
+        public string ScoringEquifaxRUsedResponcePath { get; set; }
+        public bool EquifaxEnabled { get; set; }
     }
 }
---
+++ Controllers/EquifaxScoringController.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PackageRequest;
+
+namespace Rkk2._0.Controllers
+{
+    public class EquifaxScoringController : ControllerBase
+    {
+        private readonly AppOptions _options;
+        private readonly ILogger<EquifaxScoringController>? _logger;
+        private readonly Root _listReponse;
+        private EventId _event => new EventId(new Random().Next(), nameof(EquifaxScoringController));
+
+        public EquifaxScoringController(IOptions<AppOptions> options, ILogger<EquifaxScoringController> logger)
+        {
+            _options = options.Value;
+
+            if (_options.Loging)
+            {
+                _logger = logger;
+            }
+
+            _listReponse = new Root()
+            {
+                numDirs = 1,
+                numFiles = 1,
+                path = "/",
+                isDir = true
+            };
+        }
+
+        [HttpPost]//2
+        [DisableRequestSizeLimit, RequestFormLimits(MultipartBodyLengthLimit = Int32.MaxValue, ValueLengthLimit = Int32.MaxValue), RequestSizeLimit(long.MaxValue)]
+
+        public async Task<ActionResult> Upload()
+        {
+            _logger?.LogInformation(_event, "CRE pushed request");
+            return Ok();
+        }
+
+        [HttpGet("/api/auth/get")]//1
+        public async Task<ActionResult> Auth()
+        {
+            _logger?.LogInformation(_event, "CRE asked for auth token");
+            return Ok(Guid.NewGuid());
+        }
+
+        [HttpGet("/api/resource/{filename}")]//3
+        public async Task<ActionResult> GetResource([FromRoute] string fileName)
+        {
+            _logger?.LogInformation(_event, "CRE asked for list of files");
+
+            var item = new Item()
+            {
+                path = $"/{fileName}",
+                virtualPath = $"/{fileName}",
+                name = fileName,
+                size = 214748364,
+                extension = ".XML",
+                modified = DateTime.Now,
+                isDir = false,
+                type = "blob"
+            };
+
+            var response = _listReponse;
+            response.name = fileName;
+            response.items.Add(item);
+
+            return Ok(response);
+        }
+
+
+        [HttpPost("/api/resource/{filename}")]//3
+        public async Task<ActionResult> PostResource([FromRoute] string fileName)
+        {
+            _logger?.LogInformation(_event, "CRE uploaded file");
+            return Ok();
+        }
+
+        [HttpGet]//4
+        [DisableRequestSizeLimit, RequestFormLimits(MultipartBodyLengthLimit = Int32.MaxValue, ValueLengthLimit = Int32.MaxValue), RequestSizeLimit(long.MaxValue)]
+        [Route("/api/download/{fileName}")]
+        public async Task<ActionResult> EquifaxScoringGet([FromRoute] string fileName)
+        {
+            var @event = _event;
+
+            _logger?.LogInformation(@event, $"CRE ask file {fileName}");
+
+            fileName = fileName.Replace(".reject", "");
+
+            Response.Headers.Add("Accept-Ranges", "bytes");
+
+            string[] files = Directory.GetFiles(_options.ScoringEquifaxResponcePath);
+            var responseFile = files.FirstOrDefault(x => !x.Contains("taken"));
+
+            if (files.Length == 0 || string.IsNullOrEmpty(responseFile))
+            {
+                _logger?.LogError(@event, $"Files in response folder {_options.ScoringEquifaxResponcePath} not found");
+                throw new FileNotFoundException();
+            }
+
+            var takenFile = _options.ScoringEquifaxRTakenResponcePath + Path.GetFileName(responseFile + $"_{@event.Id}");
+            System.IO.File.Move(responseFile, takenFile);
+
+            _logger?.LogInformation(@event, $"File {responseFile} is taken {takenFile}");
+
+            await Task.Delay(_options.SleepNbch);
+
+            var newResponseName = Path.Join(Path.GetDirectoryName(takenFile), fileName);
+
+            for (int retry = 0; retry <= (_options.EquifaxScoringRetryCount ?? 1);)
+            {
+                try
+                {
+                    Stream fstream = new MemoryStream();
+
+                    System.IO.File.Move(takenFile, newResponseName);
+
+                    using (var stream = new FileStream(newResponseName, FileMode.Open, FileAccess.Read))
+                    {
+                        stream.Position = 0;
+                        await stream.CopyToAsync(fstream);
+                    }
+
+                    _logger?.LogInformation(@event, $"File {newResponseName} reading success");
+
+                    System.IO.File.Move(newResponseName, _options.ScoringEquifaxResponcePath + Path.GetFileName(responseFile));
+                    _logger?.LogInformation(@event, $"Response {responseFile} is moved to used {_options.ScoringEquifaxRUsedResponcePath + Path.GetFileName(responseFile)}");
+
+                    fstream.Position = 0;
+                    return File(fstream, "application/pkcs7-mime");
+                }
+                catch (Exception ex)
+                {
+                    _logger?.LogWarning(@event, ex, $"File {fileName} fail - iteration {retry}");
+                    System.IO.File.Move(newResponseName, responseFile);
+                    _logger?.LogWarning(@event, ex, $"Taken file {fileName} is released -> {responseFile}");
+                    retry++;
+                }
+            }
+
+            throw new FileNotFoundException();
+        }
+    }
+
+    #region equifax file server
+    public class Item
+    {
+        public string path { get; set; }
+        public string virtualPath { get; set; }
+        public string name { get; set; }
+        public int size { get; set; }
+        public string extension { get; set; }
+        public DateTime modified { get; set; }
+        public int mode { get; set; }
+        public bool isDir { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Sorting
+    {
+        public string by { get; set; }
+        public bool asc { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Item> items { get; set; }
+        public int numDirs { get; set; }
+        public int numFiles { get; set; }
+        public Sorting sorting { get; set; }
+        public string path { get; set; }
+        public string virtualPath { get; set; }
+        public string name { get; set; }
+        public int size { get; set; }
+        public string extension { get; set; }
+        public DateTime modified { get; set; }
+        public int mode { get; set; }
+        public bool isDir { get; set; }
+        public string type { get; set; }
+    }
+    #endregion
+}
